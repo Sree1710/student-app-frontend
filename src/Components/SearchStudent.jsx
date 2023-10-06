0ad4e9e58a3e3360aad54e6552c58224be344655
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-06 17:53:07 +0530

    SearchStudent component created

--- src/Components/SearchStudent.jsx
+++ src/Components/SearchStudent.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SearchStudent = () => {
+    const [inputField,setInputField]=useState(
+        {rollno:""}
+    )
+
+    const inputHandler=(event)=>{
+        setInputField({...inputField,[event.target.name]:event.target.value})
+    }
+
+    const readValue=()=>{
+        console.log(inputField)
+    }
   return (
-    <div>SearchStudent</div>
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <h1>Search Student</h1>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Roll No.</label>
+                            <input onChange={inputHandler} type="text" className="form-control" name="rollno" value={inputField.rollno} />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button onClick={readValue} className="btn btn-primary">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
