185a6a6813969ca5681bc0eb358b7427c4f85a23
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-06 17:18:50 +0530

    Login component completed

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [inputField,setInputField]=useState(
+        {username:"",password:""}
+    )
+    
+    const navigate=useNavigate()
+    const inputHandler=(event)=>{
+        setInputField({...inputField,[event.target.name]:event.target.value})
+    }
+
+    const readValue=()=>{
+        console.log(inputField)
+        if (inputField.username=="admin" && inputField.password=="12345") {
+            navigate()
+        } else {
+            alert("Invalid Credentials")
+        }
+
+    }
   return (
-    <div>Login</div>
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <h1>Sign In</h1>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Username</label>
+                            <input onChange={inputHandler} type="text" className="form-control" name="username" value={inputField.username} />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Password</label>
+                            <input onChange={inputHandler} type="text" className="form-control" name="password" value={inputField.password} />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button onClick={readValue} className="btn btn-primary">Login</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
